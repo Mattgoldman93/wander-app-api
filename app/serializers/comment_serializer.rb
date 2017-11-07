713ade80ebff5752a11ab714ca22200574683177
mattgoldman93 <mattgoldman93@gmail.com>
2017-11-06 20:17:29 -0500

    Update serializer to send correct data to ember
    Serializes out everything from post except id

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,11 +1,8 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :editable, :post
+  attributes :id, :comment, :editable, :post_id
   belongs_to :user
   def editable
     scope == object.user
   end
 
-  def posts
-    object.post.pluck(:id)
-  end
 end
