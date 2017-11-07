9d5dd3b776b4ddf1f99b8dd5e796060465caf602
mattgoldman93 <mattgoldman93@gmail.com>
2017-11-06 23:45:10 -0500

    Edit migration files for heroku to build correctly

--- db/migrate/20171105184102_create_comments.rb
+++ db/migrate/20171105184102_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.text :body
+      t.text :comment
       t.references :user, foreign_key: true
       t.references :post, foreign_key: true
 
